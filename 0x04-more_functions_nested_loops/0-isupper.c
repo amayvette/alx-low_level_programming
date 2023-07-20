96f10cf49ab0b2b7817e5ab7e937faa38e8701eb
amayvette <amayvette12@gmail.com>
2023-07-20 05:28:15 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check code
+ * 
+ * Return: 1 if it is in uppercase, otherwise 0
+ */
+int _isupper(int c);
+{
+	if(c >= 65; && c<= 90)
+		return (1);
+	 else 
+		return (0);
+}
