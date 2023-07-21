d57e7c07b06c9ac76c8cfeff898ebd89c20e7e12
amayvette <amayvette12@gmail.com>
2023-07-20 19:31:41 -0700

    mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,14 @@
 #include "main.h"
 /**
- *
- *
+ * mul - is used to multiply two numbers
+ * Return: always 0
  */
-int mul(int a, int b)
+int mul(int a, int b);
 {
-	_putchar( a * b);
- 
+	printf("Enter the values of a and b");
+		Scanf("%d%d, %a%b");
+
+		int x = a*b;
+		printf("%d", x);
+		return (0);
+}
