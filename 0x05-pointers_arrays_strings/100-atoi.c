41e72b6fbc5a6a738d66633499c2cf19ccea29c5
amayvette <amayvette12@gmail.com>
2023-07-24 19:54:36 -0700

    keygen

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,19 +9,18 @@
 
 int _atoi(char *s)
 {
-
 	int i, d, n, len, f, digit;
 
 	i = 0;
 	d = 0;
 	n = 0;
 	len = 0;
-
 	f = 0;
 	digit = 0;
 
 	while (s[len] != '\0')
 		len++;
+
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
@@ -41,7 +40,6 @@ int _atoi(char *s)
 		i++;
 	}
 	if (f == 0)
-
 		return (0);
 	return (n);
 }
